15533402decefaa2c895b9014e1b2091788b059a
Your Name <you@example.com>
2022-06-08 20:28:06 +0100

    size guide page finished

--- my-app/src/Pages/SizeGuide.js
+++ my-app/src/Pages/SizeGuide.js
@@ -3,6 +3,7 @@ import background from "../img/Background_Home.png";
 import logo from "../img/logo-sem-fundo.png";
 import search_icon from "../img/icons/icons8-search.svg";
 import altair_model from "../img/MASK-HADAR.png";
+import {Link} from "react-router-dom";
 // import cart_icon from "../img/icons/icons8-shopping-bag-64.png";
 
 export default function Home() {
@@ -10,20 +11,30 @@ export default function Home() {
         <>
             <header>
                 <a>Loja</a>
-                <a href="">Size Guide</a>
-                <img class="logo" src={logo} href="home.js"></img>
-                <a>Returns</a>
+                <Link to="/sizeguide">Size Guide</Link>
+                <Link to="/"><img class="logo" src={logo} href="home.js"></img></Link>
+                <Link to="/returns">Returns</Link>
                 <a>About</a>
                 <img class="search-icon" src={search_icon}></img>
                 {/* <img class="cart-icon" src={cart_icon}></img> */}
             </header>
             <main>
-                <div class="main-container">
-                    
+                <div class="sizeguide-container">
+                    <h1>Size Guide</h1>
+                    <div class="size-img"></div>                   
                 </div>
             </main>
             <footer>
-                <div class="socials-container"></div>
+                <Link to="/"><img class="logo-footer" src={logo}></img></Link>
+                <div class="pages-ref">
+                    <Link to="/sizeguide">Size Guide</Link>
+                    <Link to="/returns">Returns</Link>
+                    <a>About</a>
+                </div>
+                <div class="socials-container">
+                    
+                </div>
+                <p>© 2021 Vieru Swimwear</p>
             </footer>
         </>
     );
