a6bd9747e8823cecb0c2d7e300abb168d443183c
Your Name <you@example.com>
2022-06-20 21:54:23 +0100

    checkout in construction

---
+++ my-app/src/Pages/Checkout.js
@@ -0,0 +1,37 @@
+import React from "react";
+import background from "../img/Background_Home.png";
+import logo from "../img/logo-sem-fundo.png";
+import search_icon from "../img/icons/icons8-search.svg";
+import altair_model from "../img/MASK-HADAR.png";
+import {Link} from "react-router-dom";
+import cart_icon from "../img/icons/icons8-shopping-bag-64.png";
+
+export default function Home() {
+    return (
+        <>
+            <header>
+                <Link to="/loja">Loja</Link>
+                <Link to="/sizeguide">Size Guide</Link>
+                <Link to="/returns">Returns</Link>
+                <Link to="/"><img class="logo" src={logo} href="home.js"></img></Link>
+                <Link to="/sobrenos">Sobre Nós</Link>
+                <img class="search-icon" src={search_icon}></img>
+                <Link to="/checkout"><img class="cart-icon" src={cart_icon}></img></Link>
+            </header>
+            <main>
+            </main>
+            <footer>
+                <Link to="/"><img class="logo-footer" src={logo}></img></Link>
+                <div class="pages-ref">
+                    <Link to="/sizeguide">Size Guide</Link>
+                    <Link to="/returns">Returns</Link>
+                    <Link to="/sobrenos">Sobre Nós</Link>
+                </div>
+                <div class="socials-container">
+                    
+                </div>
+                <p>© 2021 Vieru Swimwear</p>
+            </footer>
+        </>
+    );
+}
